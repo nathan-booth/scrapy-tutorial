935b294097732e176c9e4cdbd72a007e2ec0a3b0
Nathan Booth <boothnj@gmail.com>
2018-08-02 08:44:31 -0500

    Add initial files

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes" #  identifies the Spider.
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        """ a method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it.
+        """
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+        next_pg = response.css('li.next a::attr(href)').extract_first()
+        if next_pg is not None:
+            next_pg = response.urljoin(next_pg)
+            yield scrapy.Request(next_pg, callback=self.parse)
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        #self.log('Saved file %s' % filename)
